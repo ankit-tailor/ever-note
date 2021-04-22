ef1a868d1763a42f8cc6cdc970deac9aeded3fdc
ankit-tailor <tailorankit3345@gmail.com>
2021-04-23 00:04:10 +0530

    fix title bug

--- src/editor/editor.js
+++ src/editor/editor.js
@@ -2,73 +2,61 @@ import React, { useEffect, useState } from "react";
 import ReactQuill from "react-quill";
 import { withStyles } from "@material-ui/core/styles";
 import styles from "./styles";
-import firebase, { projectFirestore } from "../firebase/config";
+import { projectFirestore } from "../firebase/config";
 import useDebounce from "../helper";
 import BorderColorIcon from "@material-ui/icons/BorderColor";
 
-const Editor = ({ classes, selectedNote }) => {
-  const [note, setNote] = useState({
-    text: selectedNote.body,
-    title: selectedNote.title,
-    id: selectedNote.id,
-  });
+const Editor = ({ classes, note }) => {
+  const [editorNoteTitle, setEditorNoteTitle] = useState(
+    note.selectedNote.title
+  );
+  const [editorNoteBody, setEditorNoteBody] = useState(note.selectedNote.body);
+  const updateBodyDebounce = useDebounce(editorNoteBody, 1500);
+  const updateTitleDebounce = useDebounce(editorNoteTitle, 1500);
 
   useEffect(() => {
-    setNote({
-      text: selectedNote.body,
-      title: selectedNote.title,
-      id: selectedNote.id,
-    });
-  }, [selectedNote]);
-
-  const updateNoteText = useDebounce(note.text, 1500);
-  const updateNoteTitle = useDebounce(note.title, 1500);
+    setEditorNoteTitle(note.selectedNote.title);
+  }, [note.selectedNote]);
 
   useEffect(() => {
-    if (updateNoteText) {
-      projectFirestore.collection("notes").doc(selectedNote.id).update({
-        body: note.text,
-        updatedAt: firebase.firestore.FieldValue.serverTimestamp(),
+    setEditorNoteBody(note.selectedNote.body);
+  }, [note.selectedNote]);
+
+  useEffect(() => {
+    if (updateBodyDebounce) {
+      projectFirestore.collection("notes").doc(note.selectedNote.id).update({
+        body: editorNoteBody,
       });
     }
-  }, [updateNoteText]);
+  }, [updateBodyDebounce]);
 
   useEffect(() => {
-    if (updateNoteTitle) {
-      projectFirestore.collection("notes").doc(selectedNote.id).update({
-        title: note.title,
-        updatedAt: firebase.firestore.FieldValue.serverTimestamp(),
-      });
-    }
-  }, [updateNoteTitle]);
-
-  // console.log(selectedNote);
-
-  const updateNote = async (value) => {
-    await setNote({
-      ...note,
-      text: value,
+    projectFirestore.collection("notes").doc(note.selectedNote.id).update({
+      title: editorNoteTitle,
     });
+  }, [updateTitleDebounce]);
+
+  const updateNote = (text) => {
+    setEditorNoteBody(text);
   };
-
-  const updateTitle = async (e) => {
-    const text = e.target.value;
-    await setNote({ ...note, title: text });
+  const updateTitle = (e) => {
+    setEditorNoteTitle(e.target.value);
   };
-
-  // console.log(note.text);
-
   return (
     <div className={classes.editorContainer}>
       <BorderColorIcon className={classes.editIcon} />
       <input
         className={classes.titleInput}
         type="text"
+        value={editorNoteTitle}
         placeholder="Note title..."
-        value={note.title}
         onChange={updateTitle}
       />
-      <ReactQuill value={note.text} onChange={updateNote}></ReactQuill>
+      <ReactQuill
+        id="quill"
+        onChange={updateNote}
+        value={editorNoteBody}
+      ></ReactQuill>
     </div>
   );
 };
