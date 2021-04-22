77e0c9ae807f8522affcd3f9295a4b7d8d8c1e99
ankit-tailor <tailorankit3345@gmail.com>
2021-04-23 00:06:10 +0530

    change firebase intilization

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -2,14 +2,16 @@ import firebase from "firebase/app";
 import "firebase/firestore";
 
 // Initialize Firebase
-firebase.initializeApp({
+var firebaseConfig = {
   apiKey: "AIzaSyAuycjRnqMDiU4kZVu9x5483zYdv2dDiyI",
   authDomain: "ever-note-fb11e.firebaseapp.com",
   projectId: "ever-note-fb11e",
   storageBucket: "ever-note-fb11e.appspot.com",
   messagingSenderId: "1015717778700",
   appId: "1:1015717778700:web:140dc4bc42a5107bdc2529",
-});
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 const projectFirestore = firebase.firestore();
 
