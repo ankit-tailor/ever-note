1a90e4583d494ee4448a293f361782e086bb4c7e
ankit-tailor <tailorankit3345@gmail.com>
2021-04-24 02:57:08 +0530

    add authentication with email and password

---
+++ src/Authentication/Authentication.js
@@ -0,0 +1,122 @@
+import React, { useState } from "react";
+import "../Authentication/Authentication.css";
+import { projectAuth } from "../firebase/config";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+const Authentication = () => {
+  const [isSignIn, setIsSignIn] = useState(true);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const handelSubmit = (e) => {
+    e.preventDefault();
+    if (isSignIn) {
+      projectAuth
+        .signInWithEmailAndPassword(email, password)
+        .then((user) => {
+          setErrorMessage("Success");
+          setEmail("");
+          setPassword("");
+        })
+        .catch((err) => {
+          toast.error(err.message, {
+            position: "top-center",
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+          });
+        });
+    } else {
+      projectAuth
+        .createUserWithEmailAndPassword(email, password)
+        .then((user) => {
+          setErrorMessage("Success");
+          setEmail("");
+          setPassword("");
+        })
+        .catch((err) => {
+          toast.error(err.message, {
+            position: "top-center",
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+          });
+        });
+    }
+  };
+
+  return (
+    <div className="login-form">
+      <h1>Welcome to Ever Note, </h1>
+      <h2>{isSignIn ? "Sign In" : "Sign Up"}</h2>
+      <div className="form">
+        <p> </p>
+        <form className="auth-form" onSubmit={handelSubmit}>
+          <input
+            type="email"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button type="submit">{isSignIn ? "Sign In" : "Sign Up"}</button>
+          <button type="submit">Guest Login</button>
+          {isSignIn ? (
+            <p style={{ color: "black" }}>
+              New user ?{" "}
+              <span
+                style={{
+                  color: "#03203c",
+                  cursor: "pointer",
+                  textDecoration: "underline",
+                }}
+                onClick={() => {
+                  setIsSignIn(false);
+                  setErrorMessage("");
+                  setEmail("");
+                  setPassword("");
+                }}
+              >
+                Register
+              </span>
+            </p>
+          ) : (
+            <p style={{ color: "black" }}>
+              <span
+                style={{
+                  color: "#03203c",
+                  cursor: "pointer",
+                  textDecoration: "underline",
+                }}
+                onClick={() => {
+                  setIsSignIn(true);
+                  setErrorMessage("");
+                  setEmail("");
+                  setPassword("");
+                }}
+              >
+                Sing In
+              </span>
+            </p>
+          )}
+        </form>
+        <ToastContainer />
+      </div>
+    </div>
+  );
+};
+
+export default Authentication;
