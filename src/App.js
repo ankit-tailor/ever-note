3df1c8cb6567511f44fb29fcc5ee04786cb7bb9d
ankit-tailor <tailorankit3345@gmail.com>
2021-04-24 02:50:18 +0530

    add authentication

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
 import React, { useState, useEffect } from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+import Authentication from "./Authentication/Authentication";
 import Editor from "./editor/editor";
+import { projectAuth } from "./firebase/config";
 import useFirestore from "./hooks/useFirestore";
 import Sidebar from "./sidebar/sidebar";
 
 function App() {
+  const [user] = useAuthState(projectAuth);
   const [note, setNote] = useState({
     selectedNote: null,
     selectedNoteIndex: null,
     notes: null,
   });
-  const { allNotes } = useFirestore("notes");
   const { selectedNote } = note;
-
-  useEffect(() => {
-    setNote({ ...note, notes: allNotes });
-  }, [allNotes]);
 
   return (
     <div className="App">
-      <Sidebar setNote={setNote} note={note} />
-      {selectedNote && <Editor setNote={setNote} note={note} />}
+      {!user ? (
+        <Authentication />
+      ) : (
+        <div>
+          <Sidebar setNote={setNote} note={note} />
+          {selectedNote && <Editor setNote={setNote} note={note} />}
+        </div>
+      )}
     </div>
   );
 }
