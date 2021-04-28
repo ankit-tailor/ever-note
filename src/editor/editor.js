6e8c3d6b3facff1e94a2dc727c3cdea3734e7905
ankit-tailor <tailorankit3345@gmail.com>
2021-04-28 22:52:38 +0530

    add fullscreen mode

--- src/editor/editor.js
+++ src/editor/editor.js
@@ -5,11 +5,15 @@ import styles from "./styles";
 import { projectAuth, projectFirestore } from "../firebase/config";
 import useDebounce from "../helper";
 import BorderColorIcon from "@material-ui/icons/BorderColor";
+import screenfull from "screenfull";
+import FullscreenIcon from "@material-ui/icons/Fullscreen";
+import FullscreenExitIcon from "@material-ui/icons/FullscreenExit";
 
 const Editor = ({ classes, note }) => {
   const [editorNoteTitle, setEditorNoteTitle] = useState(
     note.selectedNote.title
   );
+  const [isFullscreen, setIsFullscreen] = useState(false);
   const [editorNoteBody, setEditorNoteBody] = useState(note.selectedNote.body);
   const updateBodyDebounce = useDebounce(editorNoteBody, 1500);
   const updateTitleDebounce = useDebounce(editorNoteTitle, 1500);
@@ -49,17 +53,66 @@ const Editor = ({ classes, note }) => {
     setEditorNoteTitle(e.target.value);
   };
 
+  const fullScreen = () => {
+    if (screenfull.isEnabled) {
+      setIsFullscreen(!isFullscreen);
+      screenfull.toggle(document.querySelector("#editorContainer"));
+    }
+  };
+
   const modules = {
     toolbar: [
       [{ header: [1, 2, false] }],
       ["bold", "italic", "underline", "strike", "blockquote"],
+      [
+        {
+          color: [
+            "#000000",
+            "#e60000",
+            "#ff9900",
+            "#ffff00",
+            "#008a00",
+            "#0066cc",
+            "#9933ff",
+            "#ffffff",
+            "#facccc",
+            "#ffebcc",
+            "#ffffcc",
+            "#cce8cc",
+            "#cce0f5",
+            "#ebd6ff",
+            "#bbbbbb",
+            "#f06666",
+            "#ffc266",
+            "#ffff66",
+            "#66b966",
+            "#66a3e0",
+            "#c285ff",
+            "#888888",
+            "#a10000",
+            "#b26b00",
+            "#b2b200",
+            "#006100",
+            "#0047b2",
+            "#6b24b2",
+            "#444444",
+            "#5c0000",
+            "#663d00",
+            "#666600",
+            "#003700",
+            "#002966",
+            "#3d1466",
+            "custom-color",
+          ],
+        },
+      ],
       [
         { list: "ordered" },
         { list: "bullet" },
         { indent: "-1" },
         { indent: "+1" },
       ],
-      ["link", "image"],
+      ["link", "image", "code"],
       ["clean"],
     ],
   };
@@ -67,6 +120,7 @@ const Editor = ({ classes, note }) => {
   const formats = [
     "header",
     "bold",
+    "color",
     "italic",
     "underline",
     "strike",
@@ -76,18 +130,31 @@ const Editor = ({ classes, note }) => {
     "indent",
     "link",
     "image",
+    "code",
   ];
 
   return (
-    <div className={classes.editorContainer}>
-      <BorderColorIcon className={classes.editIcon} />
-      <input
-        className={classes.titleInput}
-        type="text"
-        value={editorNoteTitle}
-        placeholder="Note title..."
-        onChange={updateTitle}
-      />
+    <div id="editorContainer" className={classes.editorContainer}>
+      <div className={classes.editorHeader}>
+        <BorderColorIcon className={classes.editIcon} />
+
+        <input
+          className={classes.titleInput}
+          type="text"
+          value={editorNoteTitle}
+          placeholder="Note title..."
+          onChange={updateTitle}
+        />
+        {!isFullscreen && (
+          <FullscreenIcon className={classes.expandIcon} onClick={fullScreen} />
+        )}
+        {isFullscreen && (
+          <FullscreenExitIcon
+            className={classes.expandIcon}
+            onClick={fullScreen}
+          />
+        )}
+      </div>
       <ReactQuill
         id="quill"
         onChange={updateNote}
